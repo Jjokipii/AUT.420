3b61a4883ae1d5bd488923e5930ba56c4de259be
Joona <joona.jokipii@tuni.fi>
2022-11-13 21:21:33 +0200

    UpdateControls() implemented

--- SellukeittoSovellus/SellukeittoSovellus/MainWindow.xaml.cs
+++ SellukeittoSovellus/SellukeittoSovellus/MainWindow.xaml.cs
@@ -45,18 +45,22 @@ namespace SellukeittoSovellus
             }).Start();
             
             InitializeComponent();
+
+            UpdateControl();
         }
 
-        public void ControlThread()
+        private void ControlThread()
         {
             try
             {
                 while (true)
                 {
-                    Console.WriteLine("ControlThread tick");
+                    //Console.WriteLine("ControlThread tick");
                     // TODO get values
 
-                    UpdateControl(State);
+                    // TODO update UI
+
+
                     
                     Thread.Sleep(THREAD_DELAY_MS);
                 }
@@ -68,31 +72,98 @@ namespace SellukeittoSovellus
         }
 
         
-        public void UpdateControl(int state)
+        private void UpdateControl()
         {
-            switch (state)
+            switch (State)
             {
                 case STATE_FAILSAFE:
+                    button_connect.IsEnabled = false;
+                    button_start_process.IsEnabled = false;
+                    button_interrupt_process.IsEnabled = false;
+                    button_set_parameters.IsEnabled = false;
+                    button_reset_parameters.IsEnabled = false;
+                    UpdateParameterControls(false);
                     break;
-
                 case STATE_DISCONNECTED:
+                    button_connect.IsEnabled = true;
+                    button_start_process.IsEnabled = false;
+                    button_interrupt_process.IsEnabled = false;
+                    button_set_parameters.IsEnabled = false;
+                    button_reset_parameters.IsEnabled = false;
+                    UpdateParameterControls(false);
                     break;
-
                 case STATE_IDLE:
+                    button_connect.IsEnabled = false;
+                    button_start_process.IsEnabled = true;
+                    button_interrupt_process.IsEnabled = false;
+                    button_set_parameters.IsEnabled = true;
+                    button_reset_parameters.IsEnabled = true;
+                    UpdateParameterControls(true);
                     break;
-
                 case STATE_RUNNING:
+                    button_connect.IsEnabled = false;
+                    button_start_process.IsEnabled = false;
+                    button_interrupt_process.IsEnabled = true;
+                    button_set_parameters.IsEnabled = false;
+                    button_reset_parameters.IsEnabled = false;
+                    UpdateParameterControls(false);
                     break;
-
                 default:
+                    Console.WriteLine("UpdateControl ERROR");
                     break;
             }
         }
 
+        private void UpdateParameterControls(bool isEnabled)
+        {
+            // Update slider controls status
+            slider_cooking_pressure.IsEnabled = isEnabled;
+            slider_cooking_temperature.IsEnabled = isEnabled;
+            slider_cooking_time.IsEnabled = isEnabled;
+            slider_saturation_time.IsEnabled = isEnabled;
+            
+            // Update textbox slider status
+            textBox_cooking_pressure.IsEnabled = isEnabled;
+            textBox_cooking_temperature.IsEnabled = isEnabled;
+            textBox_cooking_time.IsEnabled = isEnabled;
+            textBox_saturation_time.IsEnabled = isEnabled;
+        }
 
         // TODO UpdateValues
 
         private void button_start_process_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO
+
+            State = STATE_RUNNING;
+
+            UpdateControl();
+        }
+
+        private void button_interrupt_process_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO
+
+            State = STATE_FAILSAFE;
+
+            UpdateControl();
+        }
+
+        private void button_connect_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO
+
+            State = STATE_IDLE;
+
+            UpdateControl();
+        }
+
+        private void button_set_parameters_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_reset_parameters_Click(object sender, RoutedEventArgs e)
         {
 
         }
