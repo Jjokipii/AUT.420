4ddb257a54bd4843f13f8c70b7d3117d7f02334b
Joona <joona.jokipii@tuni.fi>
2022-11-13 20:42:30 +0200

    Basic ControlThread added

--- SellukeittoSovellus/SellukeittoSovellus/MainWindow.xaml.cs
+++ SellukeittoSovellus/SellukeittoSovellus/MainWindow.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading;
+using System.Windows.Threading;
 
 namespace SellukeittoSovellus
 {
@@ -19,22 +21,81 @@ namespace SellukeittoSovellus
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region CONSTANTS
+
+        private const int STATE_FAILSAFE = 0;
+        private const int STATE_DISCONNECTED = 1;
+        private const int STATE_IDLE = 2;
+        private const int STATE_RUNNING = 3;
+
+        private const int THREAD_DELAY_MS = 10;
+
+        #endregion
+
+        private int State;
+
         public MainWindow()
         {
+            State = STATE_DISCONNECTED;
+
+            new Thread(() =>
+            {
+                Thread.CurrentThread.IsBackground = true;
+                ControlThread();
+            }).Start();
+            
             InitializeComponent();
         }
+
+        public void ControlThread()
+        {
+            try
+            {
+                while (true)
+                {
+                    Console.WriteLine("ControlThread tick");
+                    // TODO get values
+
+                    UpdateControl(State);
+                    
+                    Thread.Sleep(THREAD_DELAY_MS);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        
+        public void UpdateControl(int state)
+        {
+            switch (state)
+            {
+                case STATE_FAILSAFE:
+                    break;
+
+                case STATE_DISCONNECTED:
+                    break;
+
+                case STATE_IDLE:
+                    break;
+
+                case STATE_RUNNING:
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+
+        // TODO UpdateValues
 
         private void button_start_process_Click(object sender, RoutedEventArgs e)
         {
 
         }
-
-        // TODO UpdateValues
-
-        // TODO UpdateControl
-
-        // TODO getConfiguration 
-
 
     }
 }
