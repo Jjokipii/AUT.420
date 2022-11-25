5c0bed85c3f6c6f6b5f4c3c8e3d857c411176305
simosjogren <simosjoegren@gmail.com>
2022-11-25 18:07:59 +0200

    Added State machine into Controller-class.

--- SellukeittoSovellus/SellukeittoSovellus/Controller.cs
+++ SellukeittoSovellus/SellukeittoSovellus/Controller.cs
@@ -3,18 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using System.Threading;
+using System.Windows.Threading;
 
 
 namespace SellukeittoSovellus
 {
     class Controller
     {
-        // Constants
         #region CONSTANTS
-        public const string PARAMETER_TEXTFILE_PATH = "\\default_parameter_values.txt"
+
+        public const int STATE_FAILSAFE = 0;
+        public const int STATE_DISCONNECTED = 1;
+        public const int STATE_IDLE = 2;
+        public const int STATE_RUNNING = 3;
+
+        public const string PARAMETER_TEXTFILE_PATH = "\\default_parameter_values.txt";
         #endregion
 
-        // Class variables
         #region CLASS VARIABLES
         public int default_Cooking_time;
         public int default_Cooking_temperature;
@@ -25,12 +31,104 @@ namespace SellukeittoSovellus
         public int Cooking_temperature;
         public int Cooking_pressure;
         public int Impregnation_time;
+
+        // For the state-machine
+        public int State;
+
+        #endregion
+
+        #region CONFIGURABLE PARAMETERS
+
+        private const int THREAD_DELAY_MS = 10;
+        private const string CLIENT_URL = "opc.tcp://127.0.0.1:8087";
+
+        #endregion
+
+        #region OPC
+
+        private Tuni.MppOpcUaClientLib.ConnectionParamsHolder mConnectionParamsHolder;
+        private Tuni.MppOpcUaClientLib.MppClient mMppClient;
+
         #endregion
 
         public Controller()
         {
             readDefaultParametersFromFile();
-            // TODO
+            
+            // State = STATE_DISCONNECTED;
+            State = STATE_IDLE;
+
+            // mConnectionParamsHolder = new Tuni.MppOpcUaClientLib.ConnectionParamsHolder(CLIENT_URL);
+
+            // CreateProcessConnection(); // Try to establish process connection
+
+            new Thread(() => // Start control thread
+            {
+                Thread.CurrentThread.IsBackground = true;
+                ControlThread();
+            }).Start();
+        }
+
+        public void CreateProcessConnection()
+        {
+            try
+            {
+                mMppClient = new Tuni.MppOpcUaClientLib.MppClient(mConnectionParamsHolder);
+                mMppClient.Init();
+
+                State = STATE_IDLE;
+            }
+            catch (Exception ex)
+            {
+                // TODO reset params
+                
+                Console.WriteLine(ex.Message);
+                State = STATE_DISCONNECTED;
+            }
+        }
+
+        private void ControlThread()
+        {
+            try
+            {
+                while (true) // TODO close
+                {
+                    //Console.WriteLine("{0} ControlThread tick", DateTime.Now.ToString("hh:mm:ss"));
+
+                    // TODO check connection status
+                    
+                    // Get values
+
+                    // Update controls
+
+                    // Update values
+
+                    switch (State)
+                    {
+                        case STATE_FAILSAFE:
+                            
+                            break;
+                        case STATE_DISCONNECTED:
+                            
+                            break;
+                        case STATE_IDLE:
+                            
+                            break;
+                        case STATE_RUNNING:
+
+                            break;
+                        default:
+                            Console.WriteLine("ERROR: UpdateValues() switch default statement called");
+                            break;
+                    }
+
+                    Thread.Sleep(THREAD_DELAY_MS);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
 
         private void readDefaultParametersFromFile()
@@ -88,8 +186,6 @@ namespace SellukeittoSovellus
                 Console.WriteLine("Default impregnation time loaded.");
             }
         }
-
-
         
         // TODO implement thread.
         
